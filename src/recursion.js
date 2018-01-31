00ef043f0b547f5ef0d7537bc0250e15bb99cac3
Katherine Manning <katm201@gmail.com>
2018-01-31 12:52:14 -0800

    (9 reverse) O(n) time

--- src/recursion.js
+++ src/recursion.js
@@ -107,6 +107,10 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that reverses a string.
 var reverse = function(string) {
+  if (string.length === 1) {
+    return string;
+  }
+  return string.slice(-1) + reverse(string.slice(0, -1));
 };
 
 // 10. Write a function that determines if a string is a palindrome.
