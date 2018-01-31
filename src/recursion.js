69d59483794a9ec3e133dc64262056c2d881d643
Katherine Manning <katm201@gmail.com>
2018-01-31 12:50:21 -0800

    (7 exponent) O(n) time

--- src/recursion.js
+++ src/recursion.js
@@ -78,17 +78,17 @@ var range = function(x, y) {
 // exponent(4,3); // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
-  if (exp === 1) {
-    return base;
-  }
+  //terminal condition
   if (exp === 0) {
     return 1;
   }
-  
-  const toMultiply = exp > 0 ? base : 1 / base;
-  const toExp = exp > 0 ? exp - 1 : exp + 1;
-
-  return (toMultiply * exponent(base, toExp)).toPrecision(4);
+  //solve for negative exponents
+  if (exp < 0) {
+    exp *= -1;
+    base = (1 / base);
+  }
+  //recursive condition
+  return base * exponent(base, exp - 1).toPrecision(6);
 };
 
 // 8. Determine if a number is a power of two.
